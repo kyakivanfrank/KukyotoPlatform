8168fa730ad087078af63c44b060681ce5748b58
kyakivanfrank <kyakivanfrnk@gmail.com>
2019-10-11 00:43:36 +0300

    new updatess

--- src/components/Body/Dev.js
+++ src/components/Body/Dev.js
@@ -7,10 +7,10 @@ export default ({ name, professions, writtenDetails})=> {
     
     return <div className="devbox">
 <div className="devflex ">
-<img id="devpic" src={Devpic} alt="omuziro"/>
+<img id="devpic" src={ Devpic } alt="omuziro"/>
 <div className="devstats">
 <h2 >{name}</h2>
-<div className="devProBox">{professions.map(profession => <span id="professions">{profession}</span> )}</div>
+<div className="devProBox">{ professions.map( profession =><span id="professions">{ profession }</span> ) }</div>
 </div>
 </div>
 <p id="devParagraph">{writtenDetails}</p>
