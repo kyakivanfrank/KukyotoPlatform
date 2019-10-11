a2d4be9ef203120b3352bbd714a614cdf0c04ffc
kyakivanfrank <kyakivanfrnk@gmail.com>
2019-10-11 08:43:01 +0300

    added new css

--- src/components/Body/Devsection.js
+++ src/components/Body/Devsection.js
@@ -1,11 +1,8 @@
-import React from " react";
+import React from "react";
 import Dev from "./Dev";
 
-export default ()=><section className="aboutSection">
-
-<div className="container no-skew">
-
-
+export default ()=><section className="devSection">
+    <div className="container dev-skew">
     <h2 id="DevTitle">Abajikola</h2>
     <div className="Devs">
     <Dev name="Lubwama Lincoln" professions= {["Web", "animation", "CGI"]} writtenDetails="Professionally, He’s an entry level web developer, who focuses mostly in the front-end JavaScript. He’s also artistic and uses he abilities to come up with simple but creative solutions in graphics and fine art.  
@@ -15,11 +12,9 @@ export default ()=><section className="aboutSection">
     “I felt so excited collaborating with Lincoln to make such a meaningful piece of technology, I proudly watched every step that we moved in the making. We’ve faced a bunch of challenges both past and incoming but rather have kept positive on over-coming them. I wasn’t that experience in web development to bring something like this KUKYOTO WEBSITE to life very but was motivated by his perception of the idea and have come all the way through failure, learning and success. I look forward to seeing it be important to some people who would need to use it “
     "  />
     </div>
-    <div>mpozzi ne bano wammanga beebayambako okukola project eno</div>
-
     </div>
-    
-    
+    <div className="container">mpozzi ne bano wammanga beebayambako okukola project eno</div>
+
     </section>
     
 
