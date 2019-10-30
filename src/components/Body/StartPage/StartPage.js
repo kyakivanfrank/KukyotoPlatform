6067c5ac1f29bd8e3e2e663bcfe7f0031dfff6e5
kyakivanfrank <kyakivanfrnk@gmail.com>
2019-10-30 16:40:45 +0300

    new code

--- src/components/Body/StartPage/StartPage.js
+++ src/components/Body/StartPage/StartPage.js
@@ -4,6 +4,8 @@ export default ()=> <div className="container main-tasks" >
 <div className="headingContainer">
 <h3 id="heading">Ensibuko yo Kumanya olulimi oluganda ne'bikwata ku Buganda.</h3>
 <h2 id="subheading">Kumulembe guno ogwa Yiintaneti, wonna wooli osigala okwatagana no’buwangwa bwo nga okozesa Omukutu gwaffe MUKUSOMA, OKUYIGA NO’KWEJJUKANYA     AMANYA, EBIKA, OBUJJAJJA NE’BIGAMBO byolulimi oluganda.</h2>
+
+
 </div>
 
 <div className="components">
@@ -12,20 +14,6 @@ export default ()=> <div className="container main-tasks" >
 </div>
 
 <div id="donateDIV">
-
-<button class="btn btn-warning donate-btn" data-toggle="modal" data-target=".bd-example-modal-lg">
-<strong>Tuweeyo akasiimo</strong>
-</button>
-
-<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog"
-     aria-labelledby="myLargeModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg">
-    <div class="modal-content">
-      ...
-    </div>
-  </div>
-</div>
-
 
 
 </div>
--- src/components/header/Navigation.js
+++ src/components/header/Navigation.js
@@ -7,7 +7,7 @@ const Navigation =(props)=>
 
 <header className="fixed-top" >
 <div id="rotated-div">
-<div id="anti-rotated-div" className="container2">
+<div id="anti-rotated-div" className="container">
 
 <nav className="navbar navbar-expand-lg navbar-light">
     <a className="navbar-brand" href="/">
@@ -20,7 +20,9 @@ const Navigation =(props)=>
   <ul className="nav ml-auto justify-content-center" >
 {/*	<li className="nav-item"><input className="form-control navInput"/></li>*/}
 	<li className="nav-item"><a className="nav-link active" href="/"><h3 className="navfonts">Abajikola</h3></a></li>
-	<li className="nav-item about"><a className="nav-link" href="/"><h3 className="navfonts">Ebitukwatako</h3></a></li>
+  <li className="nav-item about"><a className="nav-link" href="/"><h3 className="navfonts">Ebitukwatako</h3></a></li>
+  <li className="nav-item donate-btn"><a className="nav-link" href="/"><h3 className="navfonts">Tusigemu</h3></a></li>
+
 </ul>
   </div>
 </nav>
